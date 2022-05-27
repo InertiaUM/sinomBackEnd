88216ae86eec0501abf50418cf1206e25d7acd69
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2022-05-27 11:19:33 +0700

    Update CompanyNews.php

--- app/Models/CompanyNews.php
+++ app/Models/CompanyNews.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CompanyNews extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'news',
+    ];
+
+    /**
+     * Get the company that owns the CompanyNews
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
